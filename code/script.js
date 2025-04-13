6dc1724dc5ef8b1c243ec7f51e9087c6f59300fa
Bruno <brunoms2083@gmail.com>
2025-04-13 14:25:41 -0300

    Atualizacoes

---
+++ code/script.js
@@ -0,0 +1,8 @@
+// function MudaCor() 
+// {
+//   let cor = document.getElementById('corFundoButton').value;
+//   var mudajs = document.getElementById('tloubutjs');
+//   mudajs.style.backgroundColor = cor;
+//   mudajs.innerHTML = cor;
+// }
+// document.getElementById("buttonmudacor").addEventListener('click', MudaCor)  
